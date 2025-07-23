9007d00a7740bbbe239075ba2c0ca3137baec884
dhruv.bhuva <dhruv.bhuva@spaceo.in>
2025-07-23 18:34:43 +0530

    migration changes

--- migrations/20240101000015-add-test-id-to-pdfs.js
+++ migrations/20240101000015-add-test-id-to-pdfs.js
@@ -4,10 +4,10 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     // Add test_id column without foreign key constraint for now
-    await queryInterface.addColumn('pdfs', 'test_id', {
-      type: Sequelize.UUID,
-      allowNull: true
-    });
+    // await queryInterface.addColumn('pdfs', 'test_id', {
+    //   type: Sequelize.UUID,
+    //   allowNull: true
+    // });
 
     // Add index for better performance
     await queryInterface.addIndex('pdfs', ['test_id']);
@@ -15,6 +15,6 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
     await queryInterface.removeIndex('pdfs', ['test_id']);
-    await queryInterface.removeColumn('pdfs', 'test_id');
+    // await queryInterface.removeColumn('pdfs', 'test_id');
   }
 };
